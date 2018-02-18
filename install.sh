b6025d88de95de505c5c0eb706746c31a25f1861
mahoor13 <git@oop.ir>
2018-02-18 10:51:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-git clone https://github.com/mahoor13/squid.conf.d.git
+set -e
+cd "$(dirname "$0")"
+#git clone https://github.com/mahoor13/squid.conf.d.git
 mv /etc/squid/squid.conf /etc/squid/squid`date +"%Y%m%d%H%M%S"`.conf
 cp ./squid.conf /etc/squid/
+rm -rf /etc/squid/squid.conf.d
 mkdir /etc/squid/squid.conf.d
 cp ./*.list /etc/squid/squid.conf.d/
 cp ./adblock-updater.sh /etc/squid/squid.conf.d/
